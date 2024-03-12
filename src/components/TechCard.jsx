bad52e0cb98f3eb569cc019dfe300c3e74a2a1c6
VeeraMahendra7 <Veeramahindra787@gmail.com>
2024-03-12 20:46:30 +0530

    added

--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TechCard = ({detail}) => {
   return (
-    <div className='md:my-20 solid border bg-[#f9f9fa] hover:bg-[#c4e8fb] h-[350px] w-[280px] rounded-[8px] p-5 flex flex-col justify-center items-center shadow-xl hover:scale-105 cursor-pointer z-[-10]'>
+    <div className='md:my-20 solid border bg-[#f9f9fa] hover:bg-[#c4e8fb] md:h-[350px] h-[380px] w-[280px] rounded-[8px] p-5 flex flex-col justify-center items-center shadow-xl hover:scale-105 cursor-pointer z-[-10]'>
         <div className='h-1/2'>
             <div className='h-[100px] w-[100px] rounded-full bg-white flex justify-center items-center'>
                 <img src={detail.img} alt="technology" className='h-[50px] w-[50px] bg-contain' />
